909c4ff44cbb8892b109662f51bf7e492bf6d463
mm67686 <66744622+mm67686@users.noreply.github.com>
2020-06-11 10:43:00 -0400

    Update Github_Private.ps1

--- Github_Private.ps1
+++ Github_Private.ps1
@@ -2,6 +2,6 @@
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$Script = Invoke-RestMethod https://api.github.com/repos/Twikki/youtubetest/contents/Download_Files.ps1?access_token=yourtokenhere -Headers @{”Accept”= “application/vnd.github.v3.raw”}
+$Script = Invoke-RestMethod https://api.github.com/repos/Twikki/youtubetest/contents/Download_Files.ps1?access_token=428162d4c289ec4dcd3a400fa3112306784a7588 -Headers @{”Accept”= “application/vnd.github.v3.raw”}
 
 Invoke-Expression $Script
